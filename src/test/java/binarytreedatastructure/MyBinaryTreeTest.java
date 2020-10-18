bd143693848b7750867b3de7bb548721ca9572ad
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-18 12:45:51 +0530

    Create BST by adding 13 nodes and determine if size is equal

--- src/test/java/binarytreedatastructure/MyBinaryTreeTest.java
+++ src/test/java/binarytreedatastructure/MyBinaryTreeTest.java
@@ -18,4 +18,27 @@ public class MyBinaryTreeTest
 		
 		assertEquals(3, size);
 	}
+	
+	@Test
+	public void given13Numbers_WhenAddedToBinaryTreeShouldReturnSize13()
+	{
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		
+		int size = myBinaryTree.getSize();
+		
+		assertEquals(13, size);
+	}
 }
